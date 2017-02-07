a63cf97cf757b2f2414ba99849d4266cac38bd91
Michael Granados <michaelgranados@gmail.com>
2017-02-07 10:09:39 -0200

    installing haproxy
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- puppet_server/opt/puppet/modules/balancer/manifests/init.pp
+++ puppet_server/opt/puppet/modules/balancer/manifests/init.pp
@@ -2,6 +2,11 @@ class balancer {
   $deploy_strategy = hiera('deploy_strategy')
   $domains = $deploy_strategy['groups']['group_a']['clients']
 
+  package { 'haproxy':
+    ensure        => present,
+    allow_virtual => false,
+  }
+
   file { '/root/ips':
     owner   => root,
     group   => root,
